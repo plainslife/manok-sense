43f0fcef3daa8be36e13e1a636a48a178629459e
plainslife <matthew.alagaban@gmail.com>
2026-04-15 20:19:58 +0800

    fix: now uses two image streams, main stream for the resolution of the image that will be captured and lores for the preview in the ili9341 display fix: captured image now matches what is being displayed as preview in the ili9341 display, the resolution will still be 720p but it will be croped to 960x720 to match the what is in the preview and still get the 720p quality.

--- src/camera.py
+++ src/camera.py
@@ -7,6 +7,8 @@ from picamera2 import Picamera2
 from config import (
     CAMERA_CAPTURE_SIZE, 
     CAMERA_FORMAT, 
+    PREVIEW_W,
+    PREVIEW_H,
     CAMERA_QUALITY
 )
 
@@ -15,6 +17,7 @@ class Camera:
     def __init__(self):
         self._cam = Picamera2()
         config = self._cam.create_preview_configuration(main={"size": CAMERA_CAPTURE_SIZE, "format": CAMERA_FORMAT},
+                                                        lores={"size": (PREVIEW_W, PREVIEW_H)},
                                                         transform=Transform(rotation=180))
         self._cam.configure(config)
 
@@ -26,13 +29,21 @@ class Camera:
         self._cam.close()
 
     def grab_frame(self) -> Image.Image:
-        frame_bgr = self._cam.capture_array()
-        frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+        frame_bgr = self._cam.capture_array("lores")
+        frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_YUV2RGB_I420)
         image = Image.fromarray(frame_rgb)
 
         return image
 
     def save(self, filepath: str) -> None:
+        CROP_W = 960
         self._cam.options["quality"] = CAMERA_QUALITY
-        self._cam.capture_file(filepath)
+        # self._cam.capture_file(filepath)
+
+        # Grab the full frame and crop right side to match lores FOV
+        frame_bgr = self._cam.capture_array("main")
+        frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(frame_rgb)
+        image = image.crop((0, 0, CROP_W, 720))  # trim right side
+        image.save(filepath)
 
